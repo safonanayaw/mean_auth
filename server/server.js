1433421af58e6b6afdca17e50f690e9536e10378
safonanayaw <safoasare17@gmail.com>
2025-03-28 03:35:13 +0000

    server update

--- server/server.js
+++ server/server.js
@@ -20,6 +20,15 @@ app.use(cors({origin: allowedOrigins,
     credentials: true
 }))
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigins);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+
 //API EndPoints
 app.get('/',(req, res)=> res.send('API working'));
 
